3f581fb2a066d4724f6ece02d7f96d46a3256e6a
Cristiam Sosa <cristiam.sosa@innocv.com>
2015-08-12 19:19:22 +0200

    initial commit

---
+++ app/src/main/java/com/innocv/copysqlite/MainActivity.java
@@ -0,0 +1,49 @@
+package com.innocv.copysqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.main_text);
+
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+
+        try {
+
+            myDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            myDbHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            Log.e("MainActivity", sqle.getMessage(), sqle);
+
+        }
+
+        text.setText("base de datos creada");
+    }
+
+}
